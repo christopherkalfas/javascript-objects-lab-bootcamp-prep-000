bcb997db94f483786da48a125cf811b6d6064e54
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-29 18:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key) {
   
-  Object.assign({},recipes, {[breakfast]: value})
+  Object.assign({},rob, {[key]: value})
 }
 {
   delete [recipes.breakfast];
